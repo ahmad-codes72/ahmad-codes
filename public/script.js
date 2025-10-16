4858b53d41cecb0cf26790dd6c9be379898e6e17
ahmad-codes72 <ahmad.devdesign72@gmail.com>
2025-10-16 08:15:07 +0500

    Completed skills section

--- public/script.js
+++ public/script.js
@@ -1,42 +1,44 @@
-window.addEventListener('scroll', () => {
-    const nav = document.querySelector('nav');
-    if (window.scrollY > 50 && !nav.classList.contains('sticky')) {
-        nav.classList.add('sticky');
-        nav.classList.add('animation');
-    } else if (window.scrollY <= 50 && nav.classList.contains('sticky')) {
-        nav.classList.remove('sticky');
-        nav.classList.remove('animation')
-    };
+window.addEventListener("scroll", () => {
+  const nav = document.querySelector("nav");
+  if (window.scrollY > 50 && !nav.classList.contains("sticky")) {
+    nav.classList.add("sticky");
+    nav.classList.add("animation");
+  } else if (window.scrollY <= 50 && nav.classList.contains("sticky")) {
+    nav.classList.remove("sticky");
+    nav.classList.remove("animation");
+  }
 });
 
 function dynamicIsland() {
-    const nav = document.querySelector('nav');
-    const links = document.querySelector("ul.links");
-    nav.classList.remove('closing')
-    links.classList.add("island");
-    nav.style.gap = '0'
-    nav.classList.add("dynamic");
-};
+  const nav = document.querySelector("nav");
+  const links = document.querySelector("ul.links");
+  nav.classList.remove("closing");
+  links.classList.add("island");
+  nav.style.gap = "0";
+  nav.classList.add("dynamic");
+}
 
 function dynamicIslandRe() {
-    const nav = document.querySelector('nav');
-    const links = document.querySelector("ul.links");
-    nav.classList.add('closing')
-    nav.classList.remove('dynamic');
-    nav.style.gap = '100px';
+  const nav = document.querySelector("nav");
+  const links = document.querySelector("ul.links");
+  nav.classList.add("closing");
+  nav.classList.remove("dynamic");
+  nav.style.gap = "100px";
 
+  setTimeout(() => {
+    links.classList.remove("island");
+  }, 500);
+}
 
-    setTimeout(() => {
-        links.classList.remove('island');
-    }, 500);
-};
-
-setTimeout(dynamicIslandRe, 1000)
+setTimeout(dynamicIslandRe, 1000);
 
 setInterval(function () {
-    const nav = document.querySelector('nav');
-    const links = document.querySelector("ul.links");
-    if (!nav.classList.contains('dynamic') && !links.classList.contains('island')) {
-        dynamicIsland()
-    }
-}, 10000)
+  const nav = document.querySelector("nav");
+  const links = document.querySelector("ul.links");
+  if (
+    !nav.classList.contains("dynamic") &&
+    !links.classList.contains("island")
+  ) {
+    dynamicIsland();
+  }
+}, 10000);
