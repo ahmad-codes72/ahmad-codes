4de8c95aa627794083900d35e90b969025a643f9
ahmad-codes72 <ahmad.devdesign72@gmail.com>
2025-11-08 20:42:37 +0500

    h

--- assets/script.js
+++ assets/script.js
@@ -14,7 +14,6 @@ function dynamicIsland() {
   const links = document.querySelector("ul.links");
   nav.classList.remove("closing");
   links.classList.add("island");
-  nav.style.gap = "0";
   nav.classList.add("dynamic");
 }
 
@@ -23,7 +22,6 @@ function dynamicIslandRe() {
   const links = document.querySelector("ul.links");
   nav.classList.add("closing");
   nav.classList.remove("dynamic");
-  nav.style.gap = "100px";
 
   setTimeout(() => {
     links.classList.remove("island");
@@ -32,13 +30,3 @@ function dynamicIslandRe() {
 
 setTimeout(dynamicIslandRe, 1000);
 
-setInterval(function () {
-  const nav = document.querySelector("nav");
-  const links = document.querySelector("ul.links");
-  if (
-    !nav.classList.contains("dynamic") &&
-    !links.classList.contains("island")
-  ) {
-    dynamicIsland();
-  }
-}, 10000);
