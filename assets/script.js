27827e16f415f7f3af655eb23727346c9fee64ce
ahmad-codes72 <ahmad.devdesign72@gmail.com>
2025-11-16 16:37:56 +0500

    Completed My Personal website with Responsiveness on all devices

--- assets/script.js
+++ assets/script.js
@@ -12,10 +12,12 @@ window.addEventListener("scroll", () => {
 function dynamicIsland() {
   const nav = document.querySelector("nav");
   const links = document.querySelector("ul.links");
-  const btn = document.querySelector("nav > .sm-screen-nav > div.menu-toggle > button.icon");
+  const btn = document.querySelector(
+    "nav > .sm-screen-nav > div.menu-toggle > button.icon"
+  );
   btn.classList.remove("display");
-  nav.classList.remove("sm-expand")
-  links.classList.remove("expand")
+  nav.classList.remove("sm-expand");
+  links.classList.remove("expand");
 
   setTimeout(() => {
     nav.classList.remove("closing");
@@ -30,23 +32,26 @@ function dynamicIslandRe() {
   const links = document.querySelector("ul.links");
   nav.classList.add("closing");
   nav.classList.remove("dynamic");
-  const btn = document.querySelector("nav > .sm-screen-nav > div.menu-toggle > button.icon");
+  const btn = document.querySelector(
+    "nav > .sm-screen-nav > div.menu-toggle > button.icon"
+  );
 
-  
   setTimeout(() => {
     links.classList.remove("island");
     btn.classList.add("display");
   }, 500);
 }
 
-document.querySelector("nav > .sm-screen-nav > div.menu-toggle > button.icon").addEventListener("click", function() {
-  const nav = document.querySelector("nav")
-  const links = document.querySelector("ul.links");
-  nav.classList.toggle("sm-expand")
-  setTimeout(() => {
-    links.classList.toggle("expand")
-  }, 500);
-})
+document
+  .querySelector("nav > .sm-screen-nav > div.menu-toggle > button.icon")
+  .addEventListener("click", function () {
+    const nav = document.querySelector("nav");
+    const links = document.querySelector("ul.links");
+    nav.classList.toggle("sm-expand");
+    setTimeout(() => {
+      links.classList.toggle("expand");
+    }, 500);
+  });
 
 setTimeout(dynamicIslandRe, 1000);
 
@@ -60,3 +65,26 @@ setInterval(function () {
     dynamicIsland();
   }
 }, 10000);
+setInterval(function () {
+  if (window.innerWidth <= 575) {
+    const i = document.querySelectorAll(
+      "footer div.social-icons > div.icon > i"
+    );
+    i.forEach(function (iE) {
+      iE.classList.remove("fa-xl");
+      iE.classList.add("fa-lg");
+      return;
+    });
+  } else if (window.innerWidth > 575) {
+    const i = document.querySelectorAll(
+      "footer div.social-icons > div.icon > i"
+    );
+    i.forEach(function (iE) {
+      iE.classList.remove("fa-lg");
+      iE.classList.add("fa-xl");
+      return;
+    });
+  } else {
+    return;
+  }
+}, 300);
